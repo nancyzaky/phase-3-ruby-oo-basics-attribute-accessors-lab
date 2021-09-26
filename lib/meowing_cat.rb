c0586f0293013f4d7e0f04c9c279f04f54481e9c
Nancy Zaky <nancyzki@yahoo.com>
2021-09-26 13:09:49 -0700

    commit tree bc2ddda624175ec0c0f54b8de8d79802893612b5

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,28 @@
-## code your solution here. 
+# ## code your solution here.
+require 'pry'
+# Define a class called Cat.
+# Use the attr_accessor macro to create a setter and getter method for a cat's name.
+# Write a method, #meow, that outputs "meow!" to the terminal using the puts method when called on an instance of Cat.
+
+
+class Cat
+ def initialize(name, age=3)
+  @name = name
+  @age = age
+ end
+
+def name
+@name
+end
+
+def name=(val)
+  @name = val
+end
+  def meow
+    puts "meow!"
+  end
+
+end
+cat1 = Cat.new('hana', 4)
+
+binding.pry
